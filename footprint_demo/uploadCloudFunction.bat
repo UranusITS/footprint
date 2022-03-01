e002dc4f1e79a604668d16510de4687c24613d26
Musicminion <84625273+Musicminion@users.noreply.github.com>
2022-03-01 23:47:25 +0800

    Add files via upload

---
+++ footprint_demo/uploadCloudFunction.bat
@@ -0,0 +1,1 @@
+"D:\Program Files (x86)\Tencent\微信web开发者工具\cli.bat" cloud functions deploy --e cloud1-9gkmkayh93ab29f9 --n quickstartFunctions --r --project "C:\Users\zhangziqian\WeChatProjects\miniprogram-4" --report_first --report
